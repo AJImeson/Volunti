7ad266c1c1c9cdd9e5001f69ceea4c1814d3c6b2
mika samba <mika.samba@chasacademy.se>
2026-04-22 16:33:59 +0200

    ci/cd: Update STACK_NAME format in delete.py for consistency

--- delete.py
+++ delete.py
@@ -9,7 +9,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 PORTAINER_URL = os.getenv("PORTAINER_URL")
 API_KEY = os.getenv("PORTAINER_TOKEN")
-STACK_NAME = f"samba-{os.getenv('CI_PROJECT_NAME')}-{os.getenv('CI_COMMIT_REF_SLUG')}"
+STACK_NAME = f"volunti-{os.getenv('CI_PROJECT_NAME')}-{os.getenv('CI_COMMIT_REF_SLUG')}"
 
 headers = {
     "X-API-Key": API_KEY
