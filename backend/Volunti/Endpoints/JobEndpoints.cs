5c3db99cc5ed449f56e0ad7ab224e208614f57ab
CountKrakula <01.aliderwish@gmail.com>
2026-05-12 18:38:26 +0200

    fix: restrict DELETE /jobs to OrgAdmin only and restricte POST /jobs to OrgAdmin and OrgUser

--- backend/Volunti/Endpoints/JobEndpoints.cs
+++ backend/Volunti/Endpoints/JobEndpoints.cs
@@ -84,11 +84,11 @@ namespace Volunti.Endpoints
 
 
             })
-            .RequireAuthorization();
+            .RequireAuthorization(policy => policy.RequireRole("OrgAdmin", "OrgUser")); // Enbart OrgAdmin och OrgUser kan posta jobb
 
 
 
-            // Endpoint för att ta bort ett jobb
+            // Endpoint för att ta bort ett jobb och enbart ett jobb via id!
             app.MapDelete("/jobs/{id}", async (int id, VoluntiDbContext db, HttpContext http) =>
             {
                 var userIdClaim = http.User.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value;
@@ -120,7 +120,7 @@ namespace Volunti.Endpoints
                 return Results.Ok($"Jobb: '{job.Title}' med id: '{job.JobId}' togs bort.");
 
             })
-            .RequireAuthorization();
+            .RequireAuthorization(policy => policy.RequireRole("OrgAdmin")); // Enbart OrgAdmin kan ta bort ett jobb
 
         }
     }
