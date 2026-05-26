2dd9edd48709c0df511ba28f2e7e0541ee63888d
AJImeson <axel.imeson@chasacademy.se>
2026-05-26 19:37:15 +0200

    K3s/Dev: Testing for Development environment

--- deploy.py
+++ deploy.py
@@ -1,11 +1,13 @@
 import subprocess
 import os
 import sys
+import glob
 
 NAMESPACE = os.getenv("KUBE_NAMESPACE")
 REGISTRY = os.getenv("REGISTRY_IMAGE")
 TAG = os.getenv("CI_COMMIT_SHORT_SHA")
 COMPONENT = sys.argv[1]
+PREFIX = os.getenv("PREFIX", "")
 
 def run_script(cmd, check=True):
     print(f"$ {' '.join(cmd)}", flush=True)
@@ -14,23 +16,43 @@ def run_script(cmd, check=True):
         sys.exit(result.returncode)
     return result
 
-def apply_manifests(component):
-    run_script(["kubectl", "apply", "-f", f"K3s/base/", "-n", NAMESPACE])
-    run_script(["kubectl", "apply", "-f", "K3s/database/", "-n", NAMESPACE])
-    run_script(["kubectl", "apply", "-f", "K3s/adminer/", "-n", NAMESPACE])
-    run_script(["kubectl", "apply", "-f", f"K3s/{component}/", "-n", NAMESPACE])
-    run_script(["kubectl", "apply", "-f", "K3s/ingress/", "-n", NAMESPACE])
+def render_manifest(path): # Reads manifest files
+    with open(path) as f:
+        rendered = subprocess.run(
+                ["envsubst"],
+                input=f.read(),
+                text=True,
+                capture_output=True,
+                check=True
+        ).stdout
+    return rendered 
+
+def apply_manifest(path): # Renders a manifest by using envsubst 
+    rendered = render_manifest(path)
+    subprocess.run(
+            ["kubectl", "apply", "-f", "-", "-n", NAMESPACE],
+            input=rendered,
+            text=True,
+            check=True
+        )
+
+def apply_manifests(component): # Applies all manifests
+    for directory in ["K3s/base", "K3s/database", "K3s/adminer2",
+                      f"K3s/{component}", "K3s/ingress"]:
+        for k3s_file in sorted(glob.glob(f"{directory}/*.yaml")):
+            apply_manifest(k3s_file)
+   
 
 def create_image(component):
     image = f"{REGISTRY}/{component}:{TAG}"
     run_script(["kubectl", "set", "image",
-                f"deployment/volunti-{component}",
-                f"volunti-{component}={image}",
+                f"deployment/{PREFIX}volunti-{component}",
+                f"{PREFIX}volunti-{component}={image}",
                 "-n", NAMESPACE])
 
 def rollout_status(component):
     run_script(["kubectl", "rollout", "status",
-                f"deployment/volunti-{component}",
+                f"deployment/{PREFIX}volunti-{component}",
                 "-n", NAMESPACE, "--timeout=120s"])
 
 def main():
